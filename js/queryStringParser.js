ad198e2658330e1630faf87ec0f5d9cdb2ab83de
Pontus <pontus@wehead.se>
2013-10-15 16:42:34 +0200

    extracted function

--- js/queryStringParser.js
+++ js/queryStringParser.js
@@ -1,33 +1,39 @@
-var queryStringParser = function(queryString) {
-	if ("string" !== typeof queryString) {
-		throw new Error("Input parameter must be string");
+(function () {
+
+	function deQuestionMarkify(str) {
+		return str.charAt(0) === "?" ? str.slice(1) : str;
 	}
-
-	var ret = {};
-
-	if (queryString.charAt(0) === "?") {
-		queryString = decodeURIComponent(queryString.slice(1));
-	} else {
-		queryString = decodeURIComponent(queryString);
-	}
-
-	// extract key/value-pairs
-	queryString.split('&').forEach(function(keyVal) {
-		var keyValArr = keyVal.split('='),
-			key = keyValArr[0],
-			val = keyValArr[1];
-
-		if (/\+/.test(val)) {
-			val = val.split("+");
+	
+	var queryStringParser = function(queryString) {
+		if ("string" !== typeof queryString) {
+			throw new Error("Input parameter must be string");
 		}
 
-		if (ret[key] && Array.isArray(ret[key])) {
-			val = ret[key].concat(val);
-		}
+		var decodedString = decodeURIComponent(deQuestionMarkify(queryString)),
+			ret = {};
 
-		ret[key] = val;
+		// extract key/value-pairs
+		decodedString.split('&').forEach(function(keyVal) {
+			var keyValArr = keyVal.split('='),
+				key = keyValArr[0],
+				val = keyValArr[1];
 
-	});
+			if (/\+/.test(val)) {
+				val = val.split("+");
+			}
 
-	return ret;
-};
+			if (ret[key] && Array.isArray(ret[key])) {
+				val = ret[key].concat(val);
+			}
+
+			ret[key] = val;
+
+		});
+
+		return ret;
+	};
+
+	// expose globally
+	window.queryStringParser = queryStringParser;
+
+})();
